918414a036a53502469b3830cfbe354565382ec4
Lucsparidans <33491773+Lucsparidans@users.noreply.github.com>
2019-03-13 02:06:47 +0100

    UI updates

--- app/src/main/java/com/example/hcipartbprototype/AppointmentsFragment.java
+++ app/src/main/java/com/example/hcipartbprototype/AppointmentsFragment.java
@@ -2,10 +2,17 @@ package com.example.hcipartbprototype;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
 /**
@@ -22,7 +29,10 @@ public class AppointmentsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private CalendarView mCalendarView;
+    private TextView dateLabel;
+    private final SimpleDateFormat OUTPUT_FORMAT = new SimpleDateFormat("dd MMMM yyyy");
+    private final SimpleDateFormat INPUT_FORMAT = new SimpleDateFormat("dd MM yyyy");
 
     public AppointmentsFragment() {
         // Required empty public constructor
@@ -56,10 +66,30 @@ public class AppointmentsFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mCalendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        dateLabel = (TextView) view.findViewById(R.id.dateLabel);
+
+        dateLabel.setText(OUTPUT_FORMAT.format(mCalendarView.getDate()));
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                try {
+                    dateLabel.setText(OUTPUT_FORMAT.format(INPUT_FORMAT.parse( dayOfMonth + " " + ++month + " " + year)));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_appointments, container, false);
     }
+
+
 
 }
